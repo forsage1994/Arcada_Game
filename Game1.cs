69199f216cd8e0b24a2eaf9dba36f3c981621553
forsage1994 <mihal.4sage@gmail.com>
2020-07-18 02:57:25 +0400

    Add Player

--- Game1.cs
+++ Game1.cs
@@ -11,13 +11,16 @@ namespace Arcada1
   public class Game1 : Game
   {
     GraphicsDeviceManager graphics;
-    SpriteBatch spriteBatch;
-    Texture2D texture;
+    static SpriteBatch spriteBatch;
+    static Texture2D texture;
     Vector2 pos = new Vector2() { X = 0, Y = 0 };
-//    Int64 counter = 0;
-    sbyte dirX = 1, dirY = 1;
+    static Vector2 centerOfRotation = new Vector2() { X = 15, Y = 15 };
+    Int64 counter = 0;
+    //    sbyte dirX = 1, dirY = 1;
     Random rnd = new Random();
     Color BackgroundColor = new Color();
+    Player player = new Player(100, 100, texture);
+    //List<Target>
 
     public Game1()
     {
@@ -25,11 +28,114 @@ namespace Arcada1
       Content.RootDirectory = "Content";
     }
 
-    protected void ChangeDirection(Single Position,   ref SByte Direction,   
+    protected void ChangeDirection(Single Position, ref SByte Direction,
                                 Int32 ScreenSize, Int32 TextureSize)
     {
-      if ( (Position >= (ScreenSize - TextureSize)) || (Position <= 0) )
+      if ((Position >= (ScreenSize - TextureSize)) || (Position <= 0))
         Direction *= -1;
+    }
+
+    public enum Moves : Byte
+    {
+      Up,
+      Down,
+      Left,
+      Right
+    }
+    public enum TypesOfTarget : Byte
+    {
+      Cure,
+      Poison,
+      Death
+    }
+    public class GameObject
+    {
+      protected Texture2D Texture;
+      protected Vector2 Pos;
+      public GameObject(Int32 X, Int32 Y, Texture2D Texture)
+      {
+        this.Pos.X = X;
+        this.Pos.Y = Y;
+        this.Texture = Texture;
+      }
+      public void SetTexture(Texture2D Texture)
+      {
+        this.Texture = Texture;
+      }
+    }
+    protected class Target : GameObject
+    {
+      TypesOfTarget Type;
+      public Target(Int32 X, Int32 Y, Texture2D Texture, TypesOfTarget Type)
+        : base(X, Y, Texture)
+      {
+        this.Type = Type;
+      }
+      public void Draw()
+      {
+        spriteBatch.Begin();
+        spriteBatch.Draw(this.Texture, this.Pos, Color.White);
+        spriteBatch.End();
+      }
+    }
+    protected class Player : GameObject
+    {
+      private Int32 Lifes;
+      Moves CurrentDirection;
+      public Player(Int32 PosX, Int32 PosY, Texture2D Texture)
+        : base(PosX, PosY, Texture)
+      {
+        this.Pos.X = PosX;
+        this.Pos.Y = PosY;
+        this.Lifes = 1;
+        this.Texture = Texture;
+        this.CurrentDirection = Moves.Up;
+      }
+      public void MovePlayer(Moves Direction)
+      {
+        switch (Direction)
+        {
+          case Moves.Up:
+            this.Pos.Y -= 1;
+            this.CurrentDirection = Moves.Up;
+            break;
+          case Moves.Down:
+            this.Pos.Y += 1;
+            this.CurrentDirection = Moves.Down;
+            break;
+          case Moves.Left:
+            this.Pos.X -= 1;
+            this.CurrentDirection = Moves.Left;
+            break;
+          case Moves.Right:
+            this.Pos.X += 1;
+            this.CurrentDirection = Moves.Right;
+            break;
+        }
+      }
+
+      public void Draw()
+      {
+        spriteBatch.Begin();
+        switch (this.CurrentDirection)
+        {
+          case Moves.Up:
+            spriteBatch.Draw(this.Texture, this.Pos, null, Color.White, 0.0f, centerOfRotation, 2f, SpriteEffects.None, 0);
+            break;
+          case Moves.Down:
+            spriteBatch.Draw(this.Texture, this.Pos, null, Color.White, 3.14f, centerOfRotation, 2f, SpriteEffects.None, 0);
+            break;
+          case Moves.Left:
+            spriteBatch.Draw(this.Texture, this.Pos, null, Color.White, 3.14f * 1.5f, centerOfRotation, 2f, SpriteEffects.None, 0);
+            break;
+          case Moves.Right:
+            spriteBatch.Draw(this.Texture, this.Pos, null, Color.White, 3.14f / 2, centerOfRotation, 2f, SpriteEffects.None, 0);
+            break;
+          default:
+            break;
+        }
+        spriteBatch.End();
+      }
     }
 
     /// <summary>
@@ -41,9 +147,6 @@ namespace Arcada1
     protected override void Initialize()
     {
       // TODO: Add your initialization logic here
-      BackgroundColor.R = (byte)rnd.Next(255);
-      BackgroundColor.G = (byte)rnd.Next(255);
-      BackgroundColor.B = (byte)rnd.Next(255);
       base.Initialize();
 
     }
@@ -56,7 +159,8 @@ namespace Arcada1
     {
       // Create a new SpriteBatch, which can be used to draw textures.
       spriteBatch = new SpriteBatch(GraphicsDevice);
-      texture = Content.Load<Texture2D>("bubble");
+      texture = Content.Load<Texture2D>("tank");
+      player.SetTexture(texture);
 
       // TODO: use this.Content to load your game content here
     }
@@ -80,13 +184,32 @@ namespace Arcada1
       if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
         Exit();
 
-      pos.X += dirX;
-      pos.Y += dirY;
+      if (Keyboard.GetState().IsKeyDown(Keys.Up))
+        player.MovePlayer(Moves.Up);
 
-      ChangeDirection(pos.X, ref dirX, Window.ClientBounds.Width, texture.Width / 10);
-      ChangeDirection(pos.Y, ref dirY, Window.ClientBounds.Height, texture.Height / 10);
+      if (Keyboard.GetState().IsKeyDown(Keys.Down))
+        player.MovePlayer(Moves.Down);
+
+      if (Keyboard.GetState().IsKeyDown(Keys.Left))
+        player.MovePlayer(Moves.Left);
+
+      if (Keyboard.GetState().IsKeyDown(Keys.Right))
+        player.MovePlayer(Moves.Right);
+
+      if (counter % 600 == 0)
+      {
+
+      }
+
+      //pos.X += dirX;
+      //pos.Y += dirY;
+
+      //      ChangeDirection(pos.X, ref dirX, Window.ClientBounds.Width, texture.Width / 10);
+      //      ChangeDirection(pos.Y, ref dirY, Window.ClientBounds.Height, texture.Height / 10);
 
       base.Update(gameTime);
+
+      ++counter;
     }
 
     /// <summary>
@@ -95,10 +218,9 @@ namespace Arcada1
     /// <param name="gameTime">Provides a snapshot of timing values.</param>
     protected override void Draw(GameTime gameTime)
     {
-      GraphicsDevice.Clear(BackgroundColor);
-      spriteBatch.Begin();
-      spriteBatch.Draw(texture, pos, null, Color.White, 0.0f, Vector2.Zero, 0.1f, SpriteEffects.None, 0);
-      spriteBatch.End();
+      GraphicsDevice.Clear(Color.White);
+
+      player.Draw();
 
       // TODO: Add your drawing code here
 
--- Program.cs
+++ Program.cs
@@ -3,20 +3,20 @@
 namespace Arcada1
 {
 #if WINDOWS || LINUX
+  /// <summary>
+  /// The main class.
+  /// </summary>
+  public static class Program
+  {
     /// <summary>
-    /// The main class.
+    /// The main entry point for the application.
     /// </summary>
-    public static class Program
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            using (var game = new Game1())
-                game.Run();
-        }
+      using (var game = new Game1())
+        game.Run();
     }
+  }
 #endif
 }
